0e02b29d77822d0d856cb84a344d8e3b6691a2cd
mycho2003 <mycho2003@gmail.com>
2017-08-03 22:21:22 +0900

    Changes

---
+++ 5/5-6.py
@@ -0,0 +1,14 @@
+age = 15
+print("This person's stage of life is: \n")
+if age < 2:
+    print("Baby")
+elif 2 <= age < 4:
+    print("Toddler")
+elif 4 <= age < 13:
+    print("Kid")
+elif 13 <= age < 20:
+    print("Teenager")
+elif 20 <= age < 65:
+    print("Adult")
+else:
+    print("Elder")
